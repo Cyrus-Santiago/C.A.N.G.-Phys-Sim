09b14ac7c976f77fae4a095b80ce069b7f3888a8
FashionablyNate <nate@nathanspelts.com>
2022-02-09 14:30:11 -0800

    added mouse capture functionality

---
+++ include/input.h
@@ -0,0 +1,3 @@
+void mouseClickCallback(GLFWwindow * window, int button, int action, int mods);
+void mousePosCallback(GLFWwindow * window, double xPos, double yPos);
+void processInput(GLFWwindow *window);
