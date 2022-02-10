3c772765c2cb59141deb9fa6767610eea9fc3181
FashionablyNate <nate@nathanspelts.com>
2022-02-09 18:42:03 -0800

    added comments

--- include/input.hpp
+++ include/input.hpp
@@ -1,13 +1,20 @@
 #include <GLFW/glfw3.h>
 
+// class that stores mouse click position, mouse position, and callback functions for
+// glfw to call
 class Input {
 private:
+    // these variables record where the mouse currently is
     static double xPos, yPos;
 
 public:
+    // these variables store where the mouse was clicked
     static double xClick, yClick;
 
+    // called by glfw on mouse click
     static void mouseClickCallback(GLFWwindow * window, int button, int action, int mods);
+    // called by glfw on moust move
     static void mousePosCallback(GLFWwindow * window, double xPos, double yPos);
+    // called by glfw on keyboard input
     static void processInput(GLFWwindow *window);
 };
--- src/input.cpp
+++ src/input.cpp
@@ -4,23 +4,28 @@
 #include "../include/input.hpp"
 #include <iostream>
 
+// initialize private position coordinate variables
 double Input::xPos = 0;
 double Input::yPos = 0;
 
+// initialize public click position coordinate vatiables
 double Input::xClick = 0;
 double Input::yClick = 0;
 
+// This function is called by glfw whenever a mouse click occurs
 void Input::mouseClickCallback(GLFWwindow * window, int button, int action, int mods) {
   if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
-    //std::cout << "X=" << xPos << ", Y=" << yPos << std::endl;
-    xClick = xPos;
-    yClick = yPos;
+    // debug statement
+    std::cout << "X=" << xPos << ", Y=" << yPos << std::endl;
+    // we record the mouse click in the input class as a public variable
+    xClick = xPos; yClick = yPos;
   }
 }
 
+// this function is called by glfw whenever the moust changes position
 void Input::mousePosCallback(GLFWwindow * window, double x, double y) {
-  xPos = x;
-  yPos = y;
+  // we keep track of the position in the Input class as a private variable
+  xPos = x; yPos = y;
 }
 
 // function takes in user input and responds accordingly
