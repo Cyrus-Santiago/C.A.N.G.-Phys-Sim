6a24e6e2bd9962f74a64e9370e376b9f93ba8ea2
Amethyst <amethystskye.pi@gmail.com>
2022-02-25 19:51:43 -0800

    Fixed an oopsie

--- src/game.cpp
+++ src/game.cpp
@@ -9,24 +9,26 @@ and it should not count towards his 1000 lines. */
 #include "../include/simulationObject.hpp"
 #include "../include/ray.hpp"
 #include "../include/simulation.hpp"
+#include "../include/ecs.hpp"
 #include <glm/detail/qualifier.hpp>
 #include <iostream>
 #include <glm/fwd.hpp>
 #include <vector>
+#include <memory>
 
 playArea parea;
 playBorder pborder;
 SpriteRenderer * spriteRenderer;
 Simulation simulation;
+ECS ecs;
 std::vector<Button> Buttons;
 
-Game::Game(unsigned int width, unsigned int height) 
+Game::Game(unsigned int width, unsigned int height)
     : State(GAME_ACTIVE), Width(width), Height(height) {
-
 }
 
 Game::~Game() {
-
+  delete spriteRenderer;
 }
 
 void Game::Init() {
@@ -36,6 +38,7 @@ void Game::Init() {
   // our coordinates in easy to decipher pixel coordinates)
   glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
     static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+
   // provide the shader with the projection matrix
   ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
   Shader myShader;
@@ -45,36 +48,35 @@ void Game::Init() {
   spriteRenderer = new SpriteRenderer(myShader);
 
   // initialize menu
-  Menu::init(6, 5, Width, Height);
-  parea.init(Width, Height);
-  pborder.init(Width,Height);
+  Menu::init(6, 5, Width*0.85, Height);
+  parea.init(Width*0.85, Height);
+  pborder.init(Width*0.85,Height);
 
   // retrieve button data
   Buttons = Menu::Buttons;
   // give the button data to input class
   Input::getButtonData(Buttons);
-
   // initialize the text renderer (actually manager)
   TextRenderer::Init();
-  /* Random Simulation Objects for Testing */
   simulation.Create(glm::vec2(50, 100), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
   simulation.Create(glm::vec2(100, 100), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
+  SimulationObject simObj1 = simulation.Create(glm::vec2(150, 100));
+  SimulationObject simObj2 = simulation.Create(glm::vec2(200, 100));
   simulation.Create(glm::vec2(250, 100), glm::vec4(0.0f, 0.0f, 1.0f, 1.0f));
+  simulation.Destroy(simObj1);
+  simulation.Destroy(simObj2);
   simulation.Create(glm::vec2(150, 100), glm::vec4(1.0f, 0.0f, 1.0f, 1.0f));
   simulation.Create(glm::vec2(200, 100), glm::vec4(1.0f, 1.0f, 0.0f, 1.0f));
-
   for (Button &button : Buttons) {
-    TextRenderer::NewSentence(* spriteRenderer, 
-    button.Type + " ", glm::vec2(40, 20), 20);
+    TextRenderer::NewSentence(button.Type + " ", glm::vec2(40, 20), 20);
   }
 }
-
 void Game::Update(float dt) {
   simulation.Update(dt);
   TextRenderer::Update(dt);
 }
-
 void Game::Render() {
+  Texture2D texture = ResourceManager::GetTexture("button2");
   Ray ray({100,100});
   simulation.Create(ray);
   Buttons = Input::giveButtonData();
@@ -90,5 +92,13 @@ void Game::Render() {
     } else {
       TextRenderer::Hide(*spriteRenderer, button.Type + " ");
     }
+  }
+
+  ECS::Entity entity1 = ecs.CreateEntity();
+  entity1 = ecs.AddComponent(entity1, DIMENSIONID);
+  if (ecs.EntityHasComponent(entity1, DIMENSIONID)) {
+    spriteRenderer->DrawSprite(texture,
+      glm::vec2(ECS::EntityToComponents.at(entity1.ID).dimension.xPos,
+        ECS::EntityToComponents.at(entity1.ID).dimension.yPos));
   }
 }
