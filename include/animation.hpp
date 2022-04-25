0cbb57f1038b3c8355d1fc8fc0a7d6e03f332608
quakeroatsgod <agnellogriffen@gmail.com>
2022-04-25 13:48:57 -0700

    animations can be resized

--- include/animation.hpp
+++ include/animation.hpp
@@ -1,5 +1,7 @@
 #include "entt.hpp"
 class Animation{
 public:
-    static void updateTimeActive(entt::registry *reg, float dt);
+    void animationUpdate(entt::registry &reg, float dt);
+    void updateTimeActive(entt::registry &reg, entt::entity entt, float dt);
+    void resizeAnimation(entt::registry &reg, entt:: entity entt,float dt);
 };
