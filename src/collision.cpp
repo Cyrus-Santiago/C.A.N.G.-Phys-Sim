13cf68a31b2224a0afe9989f7de10d3e3b72e810
quakeroatsgod <agnellogriffen@gmail.com>
2022-04-26 23:13:23 -0700

    added comments while I counted lines lol

--- src/collision.cpp
+++ src/collision.cpp
@@ -344,8 +344,10 @@ void Collision::moveX(entt::registry &reg, entt::entity entt, float dt, int dire
     // get renderable component of entity
     auto enttR = reg.get<Renderable>(entt);
     if(!skip){
+        //Check if an entity exists in the direction the entity wants to move in
         auto gridEntt=entityExists(reg,entt,enttR,direction);
         if(reg.valid(gridEntt)){
+            //If there is something there, and it's a border, move in the opposite direction
             if(reg.all_of<Border>(gridEntt)){
                 if(direction % 2 ==0)    return(moveX(reg,entt,dt,3,magnitude*2,true));
                 else    return(moveX(reg,entt,dt,4,magnitude,true));
@@ -432,7 +434,7 @@ void Collision::debugGrid(SpriteRenderer &spriteRenderer, entt::registry &reg) {
 
     //This only shows the spots of entities with their associated shape. It won't
     //show every single point on the play area where an "entity" might be (in the case of bugs)
-    
+    /*
     auto view = reg.view<Renderable>();
     for(auto ent : view){
         auto enttR=reg.get<Renderable>(ent);
@@ -446,7 +448,7 @@ void Collision::debugGrid(SpriteRenderer &spriteRenderer, entt::registry &reg) {
             }
         }
     }
-    
+    */
     // sorry about the hardcoded values, loops around outside and inside of play
     // area, so you can clearly see borders and entities
     for (int x = 33; x < 816; x++) {
@@ -530,26 +532,6 @@ void Collision::destroyEnttAtLoc(entt::registry &reg, int x, int y) {
     entityUnclaim(reg, entt, enttR);
 }
 
-// void Collision::cleanBeforeMove(entt::registry *reg, entt::entity entity){
-//     auto enttR = reg->get<Renderable>(entity);
-//     for (int x = enttR.xPos - 1; x < enttR.xPos + enttR.xSize + 1; x++) {
-//         for (int y = enttR.yPos - 1; y < enttR.yPos + enttR.ySize + 1; y++) {
-//             // we ensure we're only erasing *this* component
-//             if (this->grid[x][y] == entity)
-//                 this->grid[x][y] = entt::null;
-//         }
-//     }
-// }
-
-// void Collision::updateAfterMove(entt::registry *reg, entt::entity entity){
-//     auto enttR = reg->get<Renderable>(entity);
-//     for (int x = enttR.xPos + 2; x < (enttR.xPos + enttR.xSize) - 2; x++) {
-//         for (int y = enttR.yPos + 2; y < (enttR.yPos + enttR.ySize) - 2; y++) {
-//             this->grid[x][y] = entity;
-//         }
-//     }
-// }
-
 /*
 *Arguments: entity registry, forcewave entity, delta frame time
 *Returns:   N/A
@@ -571,7 +553,6 @@ void Collision::forcewaveCollision(entt::registry &reg, entt::entity entt,float 
                 reg.destroy(entt);
                 return;
             }
-            // auto enttR=reg.get<Renderable>(gridEntt);
             //If the grid entity has physics, push it in some direction
             int rotation=reg.get<Renderable>(entt).rotate;
             //If the explosion force wave goes to the right, push right
