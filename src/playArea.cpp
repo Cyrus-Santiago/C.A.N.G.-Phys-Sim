de522bcc084c1ebe686ec5c19f2ebaf08da36274
Cyrus Santiago <cyrus.x.santiago@gmail.com>
2022-04-29 22:03:23 -0700

    Added some comments

--- src/playArea.cpp
+++ src/playArea.cpp
@@ -1,6 +1,9 @@
 //Cyrus here, this one hurt emotionally, but it works now so yay!
 #include "../include/playArea.hpp"
 
+/* Arguments: Renderer adress
+ * Returns:   N/A
+ * Purpose:   Uses the information saved by init to draw the background of the play area. */
 void PlayArea::Draw(SpriteRenderer &renderer){
     glm::vec2 pos(scrWidth*0.05, scrHeight*0.05);
     glm::vec2 size(areaWidth,areaHeight);
@@ -8,6 +11,9 @@ void PlayArea::Draw(SpriteRenderer &renderer){
     obj.Draw(renderer);
 }
 
+/* Arguments: unsigned int width, unsigned int height
+ * Returns:   N/A
+ * Purpose:   Saves and creates play area background data from the width and height of the program window. */
 void PlayArea::init(unsigned int Width, unsigned int Height){
     scrWidth=Width;
     scrHeight=Height;
